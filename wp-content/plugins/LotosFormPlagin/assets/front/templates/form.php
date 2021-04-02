3485d2a6bfd4360274442ccc86de22ba79b4d6ed
lotostoii <lotostoii@gmail.com>
2021-04-02 15:51:05 +1000

    remove wp from repository

--- wp-content/plugins/LotosFormPlagin/assets/front/templates/form.php
+++
@@ -1,8 +0,0 @@
-<div class="contract__wrapper">
-    <form action="" class="contract__form" id="contract-form">
-        <input type="text" name="first">
-        <input type="text" name="first">
-        <input type="text" name="first">
-        <input type="text" name="first">
-    </form>
-</div> 
--- wp-content/plugins/LotosFormPlagin/unistall.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (!define('WP_UNINSTALL_PLUGIN')) {
-    exit;
-}
